6d6329367cb684dd44c09d67e9aafa101f32fc52
dsedinkin <seadinkin2002@yandex.ru>
2022-07-30 09:33:39 +0500

    v1.0.0

--- index.ts
+++ index.ts
@@ -1,25 +1,22 @@
 import express from "express";
 import cors from "cors";
-import bodyParser from "body-parser";
 import axios from "axios";
 import { Tilda } from "./dist";
 
-const BASE = "http://localhost:9999/tilda", URL = "https://marinavelez.site/ddv", ID = "458530155";
+const BASE = "http://localhost:9999/tilda", URL = "http://calypso-academy.tilda.ws/", ID = "459505728";
 
-console.log(`eval(decodeURIComponent(escape(window.atob('${Buffer.from(`const log=(message="")=>{console.clear();console.log("%c"+message,"color:#fff;background-color:#fa8669;font-size:large;")};const request=(BASE="",BODY="")=>{const init={headers:{accept:"text/plain,*/*;q=0.01","accept-language":"ru","content-type":"application/x-www-form-urlencoded;charset=UTF-8"},referrer:"https://tilda.cc/",referrerPolicy:"strict-origin-when-cross-origin",body:JSON.stringify(BODY),method:"POST",credentials:"omit"},initCors=Object.assign(init,{mode:"cors"});log("Выполняется запрос на сервер...");fetch(BASE,initCors).then(res=>res.json()).then(data=>{log("Расшифровка данных полученных с сервера...");eval(decodeURIComponent(escape(window.atob(data.src))));}).catch(()=>fetch(BASE,init).then(res=>res.json()).then(data=>{log("Расшифровка данных полученных с сервера...");eval(decodeURIComponent(escape(window.atob(data.src))))}).catch(()=>log("Ошибка выполнения запроса на сервер")))};request("${BASE}",{url:"${URL}",id:${ID}});`).toString('base64')}'))));`);
+console.log(`eval(decodeURIComponent(escape(window.atob('${Buffer.from(`const log=(message="")=>{console.clear();console.log("%c"+message,"color:#fff;background-color:#fa8669;font-size:large;")};const request=(BASE="",BODY="")=>{const init={headers:{accept:"text/plain,*/*;q=0.01","accept-language":"ru","content-type":"application/json;charset=UTF-8"},referrer:"https://tilda.cc/",referrerPolicy:"strict-origin-when-cross-origin",body:JSON.stringify(BODY),method:"POST",credentials:"omit"},initCors=Object.assign(init,{mode:"cors"});log("Выполняется запрос на сервер...");fetch(BASE,initCors).then(res=>res.json()).then(data=>{log("Расшифровка данных полученных с сервера...");eval(decodeURIComponent(escape(window.atob(data.src))));}).catch(()=>fetch(BASE,init).then(res=>res.json()).then(data=>{log("Расшифровка данных полученных с сервера...");eval(decodeURIComponent(escape(window.atob(data.src))))}).catch(()=>log("Ошибка выполнения запроса на сервер")))};request("${BASE}",{url:"${URL}",id:${ID}});`).toString('base64')}'))));`);
 
 const app = express();
 const port = 9999;
 
 app.use(express.json());
 app.use(cors());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 
 try {
     app.post('/tilda', async (req, response) => {
-        const parse = JSON.parse(Object.keys(req?.body)[0]);
+        const parse = req?.body;
 
         const URL = parse.url,
             ID = parse.id,
